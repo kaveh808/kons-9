b02e971502901b809214c1aba51627c7d9d8a1b5
Kaveh Kardan <103860793+kaveh808@users.noreply.github.com>
2022-08-22 19:59:06 -1000

    3D mouse navigation working

--- sbcl/minimal-ui.lisp
+++ sbcl/minimal-ui.lisp
@@ -63,10 +63,6 @@
 (defmethod accepts-first-responder ((self scene-view))
   t)
 
-(defmethod mouse-dragged ((self scene-view) event)
-  (format t "mouse-dragged NIY...")
-  (redraw))
-
 (defun print-scene-view-help ()
   (format t "Mouse drag: orbit, [option] track left/right and up/down, [command] track in/out~%~
 `: toggle lighting~%~
@@ -85,27 +81,26 @@ delete: delete selected items ~%~
 tab: show/hide contextual menu ~%~
 h or ?: print this help message~%"))
 
-;;; update scene when key pressed
-(defmethod key-down ((self scene-view) event)
+(defmethod key-down ((self scene-view) key)
   (let* ((scene (scene self)))
-    (format t "key-down self: ~a, event: ~a~%" self event)
-    (finish-output)
-    (alexandria:switch (event :test #'equal)
-      ("h" (print-scene-view-help))
-      ("?" (print-scene-view-help))
-      ("a" (when scene (init-scene scene)))
-      ("n" (dolist (v *scene-views*) (clear-scene (scene v))))
-      ("grave-accent" (setf *do-lighting?* (not *do-lighting?*)))
-      ("1" (setf *display-filled?* (not *display-filled?*)))
-      ("2" (setf *display-wireframe?* (not *display-wireframe?*)))
-      ("3" (setf *display-points?* (not *display-points?*)))
-      ("4" (setf *do-backface-cull?* (not *do-backface-cull?*)))
-      ("5" (setf *do-smooth-shading?* (not *do-smooth-shading?*)))
-      ("6" (setf *display-ground-plane?* (not *display-ground-plane?*)))
-      ("7" (setf *display-axes?* (not *display-axes?*)))
-      ("z" (init-view-camera) (3d-update-light-settings)) ;TODO -- lights don't update when camera reset
-      ("space" (dolist (v *scene-views*) (update-scene (scene v))))
-      ("backspace" (dolist (v *scene-views*) (remove-current-selection (scene v))))
+    ;; (format t "key-down self: ~a, key: ~a~%" self key)
+    ;; (finish-output)
+    (case key
+      (:h (print-scene-view-help))
+      (:? (print-scene-view-help))
+      (:a (when scene (init-scene scene)))
+      (:n (dolist (v *scene-views*) (clear-scene (scene v))))
+      (:grave-accent (setf *do-lighting?* (not *do-lighting?*)))
+      (:1 (setf *display-filled?* (not *display-filled?*)))
+      (:2 (setf *display-wireframe?* (not *display-wireframe?*)))
+      (:3 (setf *display-points?* (not *display-points?*)))
+      (:4 (setf *do-backface-cull?* (not *do-backface-cull?*)))
+      (:5 (setf *do-smooth-shading?* (not *do-smooth-shading?*)))
+      (:6 (setf *display-ground-plane?* (not *display-ground-plane?*)))
+      (:7 (setf *display-axes?* (not *display-axes?*)))
+      (:z (init-view-camera) (3d-update-light-settings)) ;TODO -- lights don't update when camera reset
+      (:space (dolist (v *scene-views*) (update-scene (scene v))))
+      (:backspace (dolist (v *scene-views*) (remove-current-selection (scene v))))
       ))
   (redraw))
 
@@ -123,34 +118,71 @@ h or ?: print this help message~%"))
                          window))
 
 (glfw:def-key-callback key-callback (window key scancode action mod-keys)
-  (format t "key-callback: w: ~a, k: ~a, sc: ~a, a: ~a, mk: ~a ~%"
-          window key scancode action mod-keys)
-  (finish-output)
+  (declare (ignore scancode mod-keys))
+  ;; (format t "key-callback: w: ~a, k: ~a, sc: ~a, a: ~a, mk: ~a ~%"
+  ;;         window key scancode action mod-keys)
+  ;; (finish-output)
   (when (and (eq key :escape) (eq action :press))
-    (format t "XXX got ESC! Closing...")
-    (finish-output)
+    ;; (format t "XXX got ESC! Closing...")
+    ;; (finish-output)
     (glfw:set-window-should-close))
   (cond ((eq action :press)
          (pushnew key *keys-pressed*)
          (when *default-scene-view*
-           (key-down *default-scene-view* (string-downcase (string key)))))
+           (key-down *default-scene-view* key))) ;(string-downcase (string key)))))
         ((eq action :repeat)
          (when *default-scene-view*
-           (key-down *default-scene-view* (string-downcase (string key)))))
+           (key-down *default-scene-view* key))) ;(string-downcase (string key)))))
         (t (alexandria:deletef *keys-pressed* key)))
   (update-window-title window))
 
+;; TODO move to opengl.lisp
+(defparameter *current-mouse-pos-x* 0)
+(defparameter *current-mouse-pos-y* 0)
+(defparameter *current-mouse-modifier* nil)
+
 (glfw:def-mouse-button-callback mouse-callback (window button action mod-keys)
-  (format t "mouse-btn-callback: w: ~a, b: ~a, a: ~a, mk: ~a ~%"
-          window button action mod-keys)
-  (finish-output)
+  ;; (format t "mouse-btn-callback: w: ~a, b: ~a, a: ~a, mk: ~a ~%"
+  ;;         window button action mod-keys)
+  ;; (finish-output)
   (if (eq action :press)
-      (pushnew button *buttons-pressed*)
+      (let ((pos (glfw:get-cursor-position window)))
+        (pushnew button *buttons-pressed*)
+        (setf *current-mouse-pos-x* (first pos))
+        (setf *current-mouse-pos-y* (second pos))
+        (setf *current-mouse-modifier* (and mod-keys (car mod-keys)))
+        ;;        (format t "POS: ~a ~a~%" *current-mouse-pos-x* *current-mouse-pos-y*)
+        )
       (alexandria:deletef *buttons-pressed* button))
   (update-window-title window))
 
+(glfw:def-cursor-pos-callback cursor-position-callback (window x y)
+  (mouse-dragged window x y)
+  )
+
+(defun mouse-dragged (window x y)
+  (let ((action (glfw:get-mouse-button :left window)))
+    (when (eq action :press)
+      (let ((dx (- x *current-mouse-pos-x*))
+            (dy (- y *current-mouse-pos-y*)))
+        (setf *current-mouse-pos-x* x)
+        (setf *current-mouse-pos-y* y)
+        ;; (format t "mouse-dragged dx: ~a, dy: ~a, mod: ~a~%" dx dy *current-mouse-modifier*)
+        (cond ((eq :alt *current-mouse-modifier*)
+               (if (>= (abs dx) (abs dy))
+                   (incf *cam-side-dist* (* 0.1 dx))
+                   (incf *cam-up-dist* (* -0.1 dy))))
+              ((eq :super *current-mouse-modifier*)
+               (incf *cam-fwd-dist* (* 0.1 dx)))
+              (t
+               (incf *cam-x-rot* dy)
+               (incf *cam-y-rot* dx))))
+
+      (redraw))))
+
+;; TODO why is this calling ortho?
 (defun set-viewport (width height)
-  (format t "set-viewport: w: ~a, h: ~a ~%" width height)
+  ;; (format t "set-viewport: w: ~a, h: ~a ~%" width height)
   (gl:viewport 0 0 width height)
   (gl:matrix-mode :projection)
   (gl:load-identity)
@@ -158,9 +190,10 @@ h or ?: print this help message~%"))
   (gl:matrix-mode :modelview)
   (gl:load-identity))
 
+;; TODO resize window and do aspect ratio
 (glfw:def-window-size-callback window-size-callback (window w h)
-  (format t "window-size-callback: win: ~a, w: ~a, h: ~a ~%" window w h)
-  (finish-output)
+  ;; (format t "window-size-callback: win: ~a, w: ~a, h: ~a ~%" window w h)
+  ;; (finish-output)
   (setf *window-size* (list w h))
   (update-window-title window)
   (set-viewport w h))
@@ -193,6 +226,9 @@ h or ?: print this help message~%"))
            (setf %gl:*gl-get-proc-address* #'glfw:get-proc-address)
            (glfw:set-key-callback 'key-callback)
            (glfw:set-mouse-button-callback 'mouse-callback)
+
+           (glfw:set-cursor-position-callback 'cursor-position-callback)
+
            (glfw:set-window-size-callback 'window-size-callback)
            (setf *window-size* (glfw:get-window-size))
            (update-window-title glfw:*window*)
