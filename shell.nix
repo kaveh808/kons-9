34cf6d54d4902a4ac7bff937a7a83c0de02429d9
Luke Gorrie <luke@nuddy.net>
2022-09-16 11:10:05 +0200

    shell.nix: Add CL-VULKAN dependency (and SBCL init file)
    Add CL-VULKAN as a dependency.
    
    Also add an initialization file to SBCL to control settings
    like *FEATURES* when dependencies are compiled. Forgive me, this is
    currently used for tweaking CL-OPENGL settings, which are not relevant
    to the branch this commit is on... so this part of the commit is just
    following along for the ride at the moment.

--- shell.nix
+++ shell.nix
@@ -2,23 +2,41 @@
 
 # Define a lisp distribution with all the libraries that we depend on
 # (including their foreign dependencies e.g. OpenGL libraries.)
-let lisp = pkgs.lispPackages_new.sbclWithPackages (p:
-      with p; [ closer-mop
-                trivial-main-thread
-                trivial-backtrace
-                cffi
-                cl-opengl
-                cl-glu
-                cl-glfw3
-              ]); in
+let
+  # sbcl custom initialization
+  sbcl-initrc = pkgs.writeText "sbcl-initrc.lisp"
+  ''
+    ;; CL-OpenGL default behaviour is to make prohibitively expensive
+    ;; calls to mask floating point traps.
+    ;; These are already set globally in kons-9 and redundant.
+    ;; See: https://github.com/3b/cl-opengl#readme
+    (pushnew :cl-opengl-no-masked-traps *features*)
+    (pushnew :cl-opengl-no-check-error *features*)
+  '';
+  # sbcl customized startup script
+  sbcl-script = "${pkgs.sbcl}/bin/sbcl --load ${sbcl-initrc} --script";
+  # sbcl customized distribution with dependencies
+  sbcl = pkgs.lispPackages_new.lispWithPackages sbcl-script (p:
+    with p; [ closer-mop
+              trivial-main-thread
+              trivial-backtrace
+              cffi
+              cl-opengl
+              cl-glu
+              cl-glfw3
+              cl-vulkan
+            ]);
+in
 
 # Define a development environment that auto-starts this Lisp.
 pkgs.mkShell {
-  nativeBuildInputs = [ lisp ];
+  nativeBuildInputs = [ sbcl ];
   shellHook = ''
     # Start SBCL ready to run:
     #   (require :kons-9)
-    exec sbcl --eval "(require 'asdf)" --load "kons-9.asd"
+    exec sbcl --load ${sbcl-initrc} \
+              --eval "(require 'asdf)" \
+              --load "kons-9.asd"
     # (comment line above to get a bash shell with this sbcl in path.)
   '';
 }
