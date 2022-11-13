9b8417732930ccb13de9dc5604df311c3e7545cc
Michaël Le Barbier <michael.le-barbier@melusina.org>
2022-11-13 19:29:03 +0100

    Improve doctor for Confidence

--- tools/verify-confidence.lisp
+++ tools/verify-confidence.lisp
@@ -1,27 +1,46 @@
 #| Doctor for #155 https://github.com/kaveh808/kons-9/issues/155 |#
 (in-package #:cl-user)
 
-(let ((bad-confidence-quicklisp-versions
-	'("cl-confidence-20220707-git"))
-      (current-confidence-quicklisp-verion
-	(first
-	 (last
-	  (pathname-directory
-	   (ql:where-is-system "org.melusina.confidence"))))))
-  (when (member current-confidence-quicklisp-verion
-		bad-confidence-quicklisp-versions
-		:test #'string-equal )
-    (format t "~&Error: Your current version of Confidence from Quicklisp is too old.")
-    (format t "~&~%A temporary solution is to clone an up-to-date version of Confidence
+(defun quicklisp-local-projects ()
+  (car (last ql:*local-project-directories*)))
+
+(defun confidence-is-not-installed ()
+  (format t "~&Error: Confidence is not installed.")
+  (format t "~&~%A solution is to clone Confidence
+in a directory that is examined by Quicklisp. For instance using the following
+shell commands
+
+  cd ~A
+  git clone git@github.com:melusina-org/cl-confidence.git~%"
+	  (quicklisp-local-projects))
+  (quit :unix-status 1))
+
+(defun confidence-is-outdated ()
+  (format t "~&Error: Your current version of Confidence from Quicklisp is too old.")
+  (format t "~&~%A temporary solution is to clone an up-to-date version of Confidence
 in a directory that is examined by Quicklisp. For instance using the following
 shell commands
 
   cd ~A
   git clone git@github.com:melusina-org/cl-confidence.git
 
-The first line needs to be adapted if Quicklisp has been installed in an
-exotic location.
-
 Please visit https://github.com/kaveh808/kons-9/issues/155 to follow
-the discussion about this issue." "~/quicklisp/local-projects")
-    (quit :unix-status 1)))
+the discussion about this issue.~%"  (quicklisp-local-projects))
+  (quit :unix-status 1))
+
+(defun current-confidence-quicklisp-version ()
+  (let ((pathname
+	  (ql:where-is-system "org.melusina.confidence")))
+    (when pathname
+      (car (last (pathname-directory pathname))))))
+
+(let ((bad-confidence-quicklisp-versions
+	'("cl-confidence-20220707-git"))
+      (current-confidence-quicklisp-version
+	(current-confidence-quicklisp-version)))
+  (unless current-confidence-quicklisp-version
+    (confidence-is-not-installed))
+  (when (member current-confidence-quicklisp-version
+		bad-confidence-quicklisp-versions
+		:test #'string-equal )
+    (confidence-is-outdated)))
