75c0e0dd91f48edf4cee95a9b8a79e6acfee8251
Luke Gorrie <luke@nuddy.net>
2023-05-24 16:45:55 +0200

    flake.nix: Nix packaging for build & test
    Use the Nix package manager to automate some simple chores:
    
        nix run .
          run kons-9 built from local sources
    
        nix run .#test
          run kons-9 test suite
    
        nix build .#api-html
              build kons-9 API documentation in ./result/
    
    This is only a thin wrapper around the Lisp code but the significance is
    that it automatically fetches sbcl, lisp dependencies, and native
    dependencies and so it works on any machine e.g. Github Actions without
    the need to futz with installing SBCL and Quicklisp and OpenGL and ...

--- flake.nix
+++ flake.nix
@@ -1,17 +1,34 @@
-# This file defines a Nix package definition ("flake") for Kons-9.
+# This file allows the Nix package manager to operate on Kons-9.
 #
-# Nix is a package manager that can install a broad range of software on Linux
-# and macOS (but not Windows.)
+# This is primarily intended to support automated build/documentation workflows.
+# You can use it manually if you want to, though.
 #
-# Nix can install Lisp packages from Quicklisp (and other sources) and
-# automatically include their native dependencies (OpenGL libraries, etc).
+# *** Quick usage for a local checkout ***
 #
-# Nix installs all software in a separate /nix directory so that it doesn't
-# interfere with other software on the machine.
+#   nix run .
+#     run kons-9 built from local sources
+#   nix run .#test
+#     run kons-9 test suite
+#   nix build .#api-html
+#     build kons-9 API documentation in ./result/
+#
+# *** Running directly on sources in Github ***
+#
+#   nix run github:kaveh808/kons-9
+#   nix run github:kaveh808/kons-9#test
+#   nix build github:kaveh808/kons-9#api-html
+#
+# *** Prerequisites ***
+#
+#   1. Install the Nix package manager:
+#      https://nixos.org/download.html
+#   2. Enable features "flakes" and "nix-command":
+#      https://nixos.wiki/wiki/Flakes#Enable_flakes
 
 {
   description = "Kons-9 3D IDE";
-  inputs.nixpkgs.url = "nixpkgs";
+  # nixpkgs branch including mgl-pax package fixes that are still being upstreamed.
+  inputs.nixpkgs.url = "github:lukego/nixpkgs/mgl-pax";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
@@ -25,9 +42,9 @@
         # This definition also exists in the standard upstream nixpkgs
         # distribution but defining it separately here is handy for updates and
         # experiments during development.
-        kons-9 = pkgs.sbcl.buildASDFSystem rec {
+        kons-9 = pkgs.sbcl.buildASDFSystem {
           pname = "kons-9";
-          version = "trunk";
+          version = "dev";
           src = ./.;
           systems = [ "kons-9" "kons-9/testsuite"
                       "kons-9/api-types" "kons-9/api-docs" ];
@@ -43,12 +60,33 @@
           inherit kons-9;
         });
         # SBCL "distribution" including kons-9 and dependencies (both Lisp and native.)
-        lisp = sbcl'.withPackages (ps: ps.kons-9);
-        run-kons-9 = pkgs.lib.writeScript "kons-9" ''
-          ${lisp}/bin/sbcl --eval '(kons-9::run)'
+        lisp = sbcl'.withPackages (ps: [ ps.kons-9 ]);
+        # Script to run kons-9.
+        run-kons-9 = pkgs.writeScriptBin "run-kons-9" ''
+          ${lisp}/bin/sbcl --eval '(require :asdf)' \
+                           --eval '(require :kons-9)' \
+                           --eval '(kons-9::run)'
+        '';
+        # Script to run the kons-9 test suite.
+        test-kons-9 = pkgs.writeScriptBin "test-kons-9" ''
+          ${lisp}/bin/sbcl --non-interactive \
+                           --eval '(require :asdf)' \
+                           --eval '(require :kons-9/testsuite)' \
+                           --eval '(kons-9/testsuite:run-all-tests)'
+        '';
+        # API documentation in HTML format (including CSS/JS.)
+        api-html = pkgs.runCommand "api-html" {} ''
+          mkdir $out
+          ${lisp}/bin/sbcl --eval '(require :asdf)' \
+                           --eval '(require :kons-9/api-docs)' \
+                           --eval "(pax:update-asdf-system-html-docs kons-9::@api :kons-9 :target-dir \"$out/\")"
+          cd $out
+          ln -s api.html index.html
         '';
       in {
-        apps.default = { type = "app"; program = }
+        apps.default = { type = "app"; program = "${run-kons-9}/bin/run-kons-9"; };
+        apps.test    = { type = "app"; program = "${test-kons-9}/bin/test-kons-9"; };
+        packages.api-html = api-html;
       }
     );
 }
